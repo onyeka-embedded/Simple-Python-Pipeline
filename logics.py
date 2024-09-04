9d65d7745a6a576aec5dd94285b5c1da69f748ac
Onyeka_Embedded <ekwunifechris7@gmail.com>
2024-09-04 16:53:41 +0100

    reformat logics.py and main.py

--- logics.py
+++ logics.py
@@ -1,35 +1,38 @@
-#Convert letter grade to grade points.
+# Convert letter grade to grade points.
 def grade_to_points(grade):
     grade = grade.upper()
-    if grade == 'A':
+    if grade == "A":
         return 5.0
-    elif grade == 'B':
+    elif grade == "B":
         return 4.0
-    elif grade == 'C':
+    elif grade == "C":
         return 3.0
-    elif grade == 'D':
+    elif grade == "D":
         return 2.0
-    elif grade == 'E':
+    elif grade == "E":
         return 1.0
-    elif grade == 'F':
+    elif grade == "F":
         return 0.0
     else:
         return None
-    
-#Calculate CGPA based on courses taken."""
+
+
+# Calculate CGPA based on courses taken."""
 def CalculateCgpa(courses):
     total_points = 0.0
     total_credits = 0
 
     for course in courses:
-        grade_points = grade_to_points(course['grade'])
+        grade_points = grade_to_points(course["grade"])
         if grade_points is not None:
-            total_points += grade_points * course['credit']
-            total_credits += course['credit']
+            total_points += grade_points * course["credit"]
+            total_credits += course["credit"]
         else:
-            print(f"Invalid grade '{course['grade']}' entered for course {course['name']}.")
+            print(
+                f"Invalid grade '{course['grade']}' entered for course {course['name']}."
+            )
 
     if total_credits == 0:
         return 0.0  # To avoid division by zero
-    
+
     return total_points / total_credits
